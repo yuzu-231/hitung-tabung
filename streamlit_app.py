7aa1c1d304ab54bbe7f4b61e52ef080a3410174d
yuzu-231 <mrassya66@gmail.com>
2024-11-26 16:18:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.title("MENGHITUNG :blue[VOLUME TABUNG] :skull:")
+st.title("MENGHITUNG :blue[VOLUME 
+
+r = st.number_input("Masukan Jari-jari (cm) :",0)
+t = st.number_input("Masukan Tinggi (cm) :",0)
